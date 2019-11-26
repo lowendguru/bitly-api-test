f51b444ea43bb6a17affddf79b15fa458483427f
Diego Bruno <lowendguru@gmail.com>
2019-11-26 23:17:10 +0100

    cucumber skeleton

---
+++ src/test/java/com/bitly/entities/Belly.java
@@ -0,0 +1,7 @@
+package com.bitly.entities;
+
+public class Belly {
+    public void eat(int cukes) {
+
+    }
+}
---
+++ src/test/java/com/bitly/functional/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package com.bitly.functional;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "de.monochromata.cucumber.report.PrettyReports:target/cucumber" })
+public class RunCucumberTest {
+}
